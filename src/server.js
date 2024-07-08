a1f4ac4749ebb993c924c7fb8cf5b0ab000d3a55
kimgi <kimgi@ais-info.co.jp>
2024-07-08 14:08:36 +0900

    client0708002

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import {io} from 'socket.io-client'
-const server = io('http://localhost:5000/')
-//const server = io('https://chat-server-ig3y.onrender.com')
+// const server = io('http://localhost:5000/')
+// const server = io('https://chat-server-ig3y.onrender.com')
 // const server = io('https://chatapp-server-txz2.onrender.com');
-// const server = io('https://chatapp-server-txz2.onrender.com', {
-//     reconnection: true,
-//     reconnectionAttempts: Infinity,
-//     reconnectionDelay: 1000,
-//     reconnectionDelayMax: 5000,
-//     timeout: 20000
-//   });
+const server = io('https://chatapp-server-txz2.onrender.com', {
+    reconnection: true,
+    reconnectionAttempts: Infinity,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 5000,
+    timeout: 20000
+  });
 
 export default server

368c23cf8f899db10bde26a5434234ebad027af8
kimgi <kimgi@ais-info.co.jp>
2024-07-12 11:18:04 +0900

    client0712001

--- src/App.js
+++ src/App.js
@@ -1,439 +1,3 @@
-// import React,{ useState, useEffect } from "react"; 
-// import "./App.css";
-// import socket from "./utils/socket";
-// import InputField from "./components/InputField/InputField";
-// import MessageContainer from "./components/MessageContainer/MessageContainer";
-
-// function App() {
-
-//   const [user, setUser] = useState(""); // 유저 정보 저장
-//   const [message, setMessage] = useState(''); // 메세지 저장 
-//   const [messageList, setMessageList] = useState([]);
-//   console.log("message List", messageList);
-
-//   const askUserName=()=>{
-//     const userName = prompt("이름을 입력하세요.");
-//     setUser(userName);
-//     console.log("userName:", userName);
-
-//     socket.emit("login", userName, (res) => {
-//       console.log("Res", res);
-//       if(res?.ok) {
-//         setUser(res.data);
-//       }
-//     });
-//   };
-
-//   useEffect(() => {
-//     socket.on("message", (message) => {
-//       console.log("message", message);
-//       setMessageList((prevMessages) => [...prevMessages, message]);
-//     });
-//     askUserName();
-
-//     return () => {
-//       socket.off("message");
-//     };
-//   }, []); // 빈 배열로 설정하여 한 번만 실행되도록 함
-
-//   const sendMessage = (event) => {
-//     event.preventDefault();
-//     socket.emit("sendMessage", message, (res) => {
-//       console.log("sendMessage res", res);
-//       if(res.ok) {
-//         setMessage("");
-//       }
-//     });
-//     //setMessageList((prevState) => prevState.concat(message));
-//     setMessageList((prevState) => prevState.concat({ user: { name: user }, chat: message }));
-//     setMessage(""); // 메세지 전송 후 입력 필드 비우기
-//   };
-  
-//   return (
-//     <div>
-//       <div className="App">
-//         <MessageContainer messageList={messageList} user={user} />
-//         <InputField 
-//           message={message} 
-//           setMessage={setMessage} 
-//           sendMessage={sendMessage}
-//         />
-//       </div>
-//     </div>
-//   );
-// }
-
-
-// export default App;
-
-// import React, { useEffect, useRef ,useState } from "react";
-// import server from "./server";
-// import "./App.css";
-
-// function App() {
-//   const [isLogin, setIsLogin] = useState(false); // 로그인 상태 저장
-//   const [nickName, setNickName] = useState(""); // 닉네임 저장
-//   const [msg, setMsg] = useState([]); // 메시지 리스크 저장
-
-//   const msg_ref = useRef(""); // 메시지 입력 필드 참조
-//   const nickname_ref = useRef(""); // 닉네임 입력 필드 참조
-//   const scroll_ref = useRef(0);  // 채팅 창 스크롤 참조
-//   const video_ref = useRef(null);
-
-//   function handleLoginClick() {
-//     if(nickname_ref.current && nickname_ref.current.value) {
-//       server.emit('login', nickname_ref.current.value);
-//       setIsLogin(true);
-//       setNickName(nickname_ref.current.value);
-//     }
-//   }
-
-//   function handleLoginKeyClick(e) {
-//     if(e.key === 'Enter') {
-//       handleLoginClick();
-//     }
-//   }
-
-//   function handleSendClick() {
-//     if(msg_ref.current.value !== "") {
-//       let msg_array = [...msg]
-//       msg_array.push({level:"me", msg:msg_ref.current.value});
-//       setMsg(msg_array);
-//       server.emit('send', {nickName:nickName, msg:msg_ref.current.value});
-//       msg_ref.current.value = "";
-//     }
-//   }
-
-//   function handleSendKeyClick(e) {
-//     if(e.key === 'Enter') {
-//       handleSendClick();
-//     }
-//   }
-
-//   useEffect(() => {
-//     if(scroll_ref.current) {
-//       scroll_ref.current.scrollTop = scroll_ref.current.scrollHeight;
-//     }
-
-//     server.on('msg', (data) => {
-//       let msg_array = [...msg];
-//       msg_array.push(data);
-//       setMsg(msg_array);
-//     });
-
-//     if(msg_ref.current && msg[msg.length -1].level === "me") {
-//       msg_ref.current.value = "";
-//     }
-//   }, []);
-
-//   useEffect(() => {
-//     const video = video_ref.current;
-
-//     if(video) {
-//       const handleVideoEnd = () => {
-//         video.currentTime = 0;
-//         video.play();
-//       };
-
-//       video.addEventListener('ended', handleVideoEnd);
-
-//       return () => {
-//         video.removeEventListener('ended', handleVideoEnd);
-//       };
-//     };
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       {!isLogin ?
-//       <div className="login">
-//         {/* <video ref={video_ref} autoPlay muted loop>
-//           <source src="/하늘.mp4" type="video/mp4" />
-//         </video> */}
-//         <div className="login_wrapper">
-//           <div className="name">名前を入力してください <input ref={nickname_ref} onKeyDown={handleLoginKeyClick}></input></div>
-//           <button onClick={handleLoginClick} className="enter_btn"></button>
-//         </div>
-//       </div>
-//       :
-//       <div className="chatting">
-//         <div className="chatting_wrapper">
-//           <div ref={scroll_ref} className='chatting_box'>
-//             {msg.map(c=>{
-//               return <div className='msg_box'
-//                   style={{
-//                     justifyContent: c.level == "sys" ? "center" : c.level == "" ? "start" : "end"}}>
-//                     <div className={c.level == "sys" ? 'msg_center' : "msg"}>{c.msg}</div>
-//                     <div className='nickname_style'>{c.nickName}</div> {/* 메세지 보낸 사람 닉네임 표시 */}
-                    
-//                 </div>
-//             })
-//             }
-//           </div>
-
-//           <div className="input_box">
-//             <input ref={msg_ref} onKeyDown={handleSendKeyClick}></input>
-//             <div className="box"></div>
-//             <button onClick={handleSendClick}>send</button>
-//           </div>
-//         </div>
-//       </div>  
-//     }
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// import React, { useEffect, useRef, useState } from "react";
-// import server from "./server";
-// import CustomDate from "./CustomDate";
-// import Sidebar from "./Sidebar";
-// import "./App.css";
-// import "./Sidebar.css";
-
-
-// function App() {
-//   const [isLogin, setIsLogin] = useState(false); // 로그인 상태 저장
-//   const [nickName, setNickName] = useState(""); // 닉네임 저장
-//   const [msg, setMsg] = useState([]); // 메시지 리스트 저장
-//   const [dateMsg, setDateMsg] = useState([]);
-//   const [userList, setUserList] = useState([]); // 사용자 목록 저장
-
-//   const msg_ref = useRef(""); // 메시지 입력 필드 참조
-//   const nickname_ref = useRef(""); // 닉네임 입력 필드 참조
-//   const scroll_ref = useRef(null);  // 채팅 창 스크롤 참조
-
-
-//   function handleLoginClick() {
-//     if (nickname_ref.current && nickname_ref.current.value) {
-//       setMsg([]); //로그인 시 채팅창 초기화 
-//       server.emit('login', nickname_ref.current.value);
-//       setIsLogin(true);
-//       setNickName(nickname_ref.current.value);
-//     }
-//   }
-
-//   function handleLoginKeyClick(e) {
-//     if (e.key === 'Enter') {
-//       handleLoginClick();
-//     }
-//   }
-
-//   function handleSendClick() {
-//     if (msg_ref.current.value !== "") {
-//       let msg_array = [...msg]
-//       msg_array.push({ level: "me", msg: msg_ref.current.value });
-//       setMsg(msg_array);
-//       server.emit('sendMessage', { nickName: nickName, msg: msg_ref.current.value });
-//       msg_ref.current.value = "";
-//     }
-//   }
-
-//   function handleSendKeyClick(e) {
-//     if (e.key === 'Enter') {
-//       handleSendClick();
-//     }
-//   }
-
-//   // useEffect(() => {
-//   //   const handleMessage = (data) => {
-//   //     setMsg(prevMsg => [...prevMsg, data]);
-//   //   };
-//   useEffect(() => {
-//     const handleMessage = (data) => {
-//       if (data.level === "date") {
-//         setDateMsg(data.msg);
-//       } else {
-//         setMsg(prevMsg => [...prevMsg, data]);
-//       }
-//     };
-
-//     const handleUpdateUserList = (users) => {
-//       setUserList(users);
-//     };
-
-//     server.on('msg', handleMessage);
-//     server.on('updateUserList', handleUpdateUserList);
-
-//     return () => {
-//       server.off('msg', handleMessage);
-//       server.off('updateUserList', handleUpdateUserList);
-//     };
-//   }, []);
-
-//   useEffect(() => {
-//     if (scroll_ref.current) {
-//       scroll_ref.current.scrollTop = scroll_ref.current.scrollHeight;
-//     }
-//   }, [msg, dateMsg]);
-
-//   return (
-//     <div className="App">
-//       {!isLogin ?
-//         <div className="login">
-//           <div className="login_wrapper">
-//             <div className="name">名前を入力してください <input ref={nickname_ref} onKeyDown={handleLoginKeyClick}></input></div>
-//             <button onClick={handleLoginClick} className="enter_btn"></button>
-//           </div>
-//         </div>
-//         :
-//         <div className="chat-container">
-//           <Sidebar userList={userList} />
-//         <div className="chatting">
-//           <div className="chatting_wrapper">
-//             <div ref={scroll_ref} className='chatting_box'>
-//               <div><CustomDate /></div>
-//               {msg.map((c, index) => (
-//                 <div key={index} className='msg_box' 
-//                   style={{
-//                     justifyContent: c.level === "sys" ? "center" : c.level === "me" ? "end" : "start"
-//                   }}>
-//                     {c.level !== "sys" && c.level !== 'me' && <img src="/몰티즈4.png" alt="" className="avatar"/>}
-//                   <div className={c.level === "sys" ? 'msg_center' : c.level === "me" ? "msg_me" : "msg_other"}>{c.msg}</div>
-//                   {c.level !== "sys" && <div className='nickname_style'>{c.nickName}</div>}
-//                 </div>
-//               ))}
-//             </div>
-            
-
-//             <div className="input_box">
-//               <input ref={msg_ref} onKeyDown={handleSendKeyClick}></input>
-//               <div className="box"></div>
-//               <button onClick={handleSendClick}></button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       }
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-// import React, { useEffect, useRef, useState } from "react";
-// import server from "./server";
-// import CustomDate from "./components/CustomDate";
-// import Sidebar from "./components/Sidebar/Sidebar"; // 파일명과 일치시킴
-// import "./App.css";
-// import "./components/Sidebar/Sidebar.css";
-
-// function App() {
-//   const [isLogin, setIsLogin] = useState(false); // 로그인 상태 저장
-//   const [nickName, setNickName] = useState(""); // 닉네임 저장
-//   const [msg, setMsg] = useState([]); // 메시지 리스트 저장
-//   const [dateMsg, setDateMsg] = useState([]);
-//   const [userList, setUserList] = useState([]); // 사용자 목록 저장
-
-//   const msg_ref = useRef(""); // 메시지 입력 필드 참조
-//   const nickname_ref = useRef(""); // 닉네임 입력 필드 참조
-//   const scroll_ref = useRef(null);  // 채팅 창 스크롤 참조
-
-//   function handleLoginClick() {
-//     if (nickname_ref.current && nickname_ref.current.value) {
-//       setMsg([]); // 로그인 시 채팅창 초기화 
-//       server.emit('login', nickname_ref.current.value);
-//       setIsLogin(true);
-//       setNickName(nickname_ref.current.value);
-//     }
-//   }
-
-//   function handleLoginKeyClick(e) {
-//     if (e.key === 'Enter') {
-//       handleLoginClick();
-//     }
-//   }
-
-  // function handleSendClick() {
-    // if (msg_ref.current.value !== "") {
-    //   let msg_array = [...msg];
-    //   msg_array.push({ level: "me", msg: msg_ref.current.value });
-    //   // setMsg(msg_array);
-    //   server.emit('sendMessage', { nickName: nickName, msg: msg_ref.current.value });
-    //   msg_ref.current.value = "";
-  //   }
-  // }
-
-//   function handleSendKeyClick(e) {
-//     if (e.key === 'Enter') {
-//       handleSendClick();
-//     }
-//   }
-
-//   useEffect(() => {
-//     const handleMessage = (data) => {
-//       if (data.level === "date") {
-//         setDateMsg(data.msg);
-//       } else {
-//         setMsg(prevMsg => [...prevMsg, data]);
-//       }
-//     };
-
-//     const handleUpdateUserList = (users) => {
-//       setUserList(users);
-//       console.log('users >> ', users);
-//     };
-
-//     server.on('msg', handleMessage);
-//     server.on('updateUserList', handleUpdateUserList);
-
-//     return () => {
-//       server.off('msg', handleMessage);
-//       server.off('updateUserList', handleUpdateUserList);
-//     };
-//   }, []);
-
-//   useEffect(() => {
-//     if (scroll_ref.current) {
-//       scroll_ref.current.scrollTop = scroll_ref.current.scrollHeight;
-//     }
-//   }, [msg, dateMsg]);
-
-//   return (
-//     <div className="App">
-//       {!isLogin ?
-//         <div className="login">
-//           <div className="login_wrapper">
-//             <div className="name">名前を入力してください <input ref={nickname_ref} onKeyDown={handleLoginKeyClick}></input></div>
-//             <button onClick={handleLoginClick} className="enter_btn"></button>
-//           </div>
-//         </div>
-//         :
-        // <div className="chat-container">
-        //   <Sidebar userList={userList} />
-        //   <div className="chatting">
-        //     <div className="chatting_wrapper">
-        //       <div ref={scroll_ref} className='chatting_box'>
-        //         <div><CustomDate /></div>
-        //         {msg.map((c, index) => (
-        //           <div key={index} className='msg_box' 
-        //             style={{
-        //               justifyContent: c.level === "sys" ? "center" : c.level === "me" ? "end" : "start"
-        //             }}>
-        //             {c.level !== "sys" && c.level !== 'me' && <img src="/몰티즈4.png" alt="" className="avatar"/>}
-        //             <div className={c.level === "sys" ? 'msg_center' : c.level === "me" ? "msg_me" : "msg_other"}>{c.msg}</div>
-        //             {c.level !== "sys" && c.level !== 'me' && <div className='nickname_style'>{c.nickName}</div>}
-        //           </div>
-        //         ))}
-        //       </div>
-        //       <div className="input_box">
-        //         <input ref={msg_ref} onKeyDown={handleSendKeyClick}></input>
-        //         <div className="box"></div>
-        //         <button onClick={handleSendClick}></button>
-        //       </div>
-        //     </div>
-        //   </div>
-        // </div>
-//       }
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React, { useEffect, useRef, useState } from "react";
 import server from "./server";
 import ChatRoomList from "./components/ChatRoom/ChatRoomList";
@@ -474,13 +38,17 @@ function App() {
   };
 
   const handleJoinRoom = (roomName) => {
+    if (currentRoom === roomName) {
+      alert("이미 입장되어 있습니다.");
+      return;
+    }
     console.log(`Join room: ${roomName}`);
-    if(currentRoom) {
+    if (currentRoom) {
       server.emit('leaveRoom', { roomName: currentRoom, nickName });
     }
-    server.emit('joinRoom', { roomName, nickName });
-    setMessages([]);
     setCurrentRoom(roomName);
+    setMessages([]);
+    server.emit('joinRoom', { roomName, nickName });
   };
 
   const handleCreateRoom = (roomName) => {
@@ -489,13 +57,15 @@ function App() {
   };
 
   const handleDeleteRoom = ({ roomName, requestedBy }) => {
-    const roomToDelete = roomList.find(room => room.roomName === roomName);
+    if(window.confirm("정말로 삭제하시겠습니까?")) {
+      const roomToDelete = roomList.find(room => room.roomName === roomName);
     if (roomToDelete && roomToDelete.createdBy === requestedBy) {
       setRoomList(roomList.filter(room => room.roomName !== roomName));
       server.emit('deleteRoom', { roomName, requestedBy });
     } else {
       alert("방을 삭제할 권한이 없습니다.");
     }
+    };
   };
 
   const handleSendMessage = (message) => {
@@ -523,6 +93,7 @@ function App() {
     const handleJoinRoomSuccess = ({ roomName }) => {
       console.log(`Joined room: ${roomName}`);
       setCurrentRoom(roomName);
+      setMessages([]);
     };
 
     const handleJoinRoomFail = ({ msg }) => {
@@ -542,6 +113,8 @@ function App() {
     const handleRoomDeleted = (data) => {
       alert(data.msg);
       setCurrentRoom("");
+      setShowRoomList(false);
+      setMessages([]);
     };
 
     server.on('msg', handleMsg);
@@ -607,4 +180,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -7,7 +7,7 @@ function Sidebar({ userList, nickName }) {
       <div className='currentAccessPerson'>현재 접속자🐾</div>
       {userList.map((user, index) => (
         <div key={index} className={`user ${user.nickName === nickName ? 'current-user' : ''}`}>
-          {user.nickName}
+          {user.nickName} {user.roomName ? `(${user.roomName})` : ''}
         </div>
       ))}
     </div>
